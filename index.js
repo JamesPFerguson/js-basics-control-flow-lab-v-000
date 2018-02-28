583d772181846b493b55865f497ce934e52f3861
James Ferguson <ncjamesferguson@gmail.com>
2018-02-28 06:27:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function scuberGreetingForFeet (ride)
   else if (ride > 2000) {
     return "I will gladly take your thirty bucks."
   }
-  else if (ride > 2500) {
+  else if (ride >= 2500) {
     return 'No can do.'
   }
 }
